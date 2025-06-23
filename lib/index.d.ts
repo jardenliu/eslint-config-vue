d30b4e69ba817ff53302597614c066f697f66a25
流君酱 <15915839707@163.com>
2025-06-23 14:49:48 +0800

    chore: 修改编译配置

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import EslintTs from 'typescript-eslint';
-
-type InferredConfigType = ReturnType<typeof EslintTs.config>;
-declare const eslintConfig: InferredConfigType;
-
-declare const legacyConfig: InferredConfigType;
-
-export { eslintConfig as default, legacyConfig };
-export type { InferredConfigType };
--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,29 @@
-import typescript  from '@rollup/plugin-typescript';
-
-import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 
 const config = [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'lib/index.cjs',
-        format: 'cjs',
-      },
-      {
-        file: 'lib/index.mjs',
-        format: 'esm',
-      },
-    ],
-    plugins: [typescript()],
-  },
-  {
-    input: 'src/index.ts',
-    output: { file: 'lib/index.d.ts', format: 'esm' },
-    plugins: [dts()],
-  },
-];
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'lib/index.cjs',
+                format: 'cjs',
+            },
+            {
+                file: 'lib/index.js',
+                format: 'esm',
+            }
+        ],
+        plugins: [typescript()],
+    },
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'lib/index.d.ts',
+            format: 'esm',
+        },
+        plugins: [dts()],
+    }
+]
 
- export default config;
+export default config
